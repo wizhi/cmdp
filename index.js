ceb90946e0dd31ed2c82ad50c54c308ddcbfad7a
Simon Bo Dall Mikkelsen <xwizhi@gmail.com>
2017-08-27 02:55:09 +0200

    Allow for parsing directly from the commandline

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ module.exports = {
     lex,
     parse
 };
+
+if (require.main === module) {
+    const input = process.argv[2];
+
+    if (input === undefined) {
+        console.log("Usage: ./index.js <command>");
+        return;
+    }
+
+    const tokens = lex(input);
+    const command = parse(tokens);
+    
+    console.log(command);
+}
